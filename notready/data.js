1d07a6ce296494f97b0b3d72b8ce5b45dfba97de
test-git <imnotgayyyyy@gmail.com>
2020-05-07 12:39:05 +0200

    -

---
+++ notready/data.js
@@ -0,0 +1,103 @@
+var data = {
+  "works":[
+
+  
+
+    {
+
+      "v_titles": "jiamin zhangs",
+      "v_description": "Jiamin Zhang Happy Improvisation112018",
+
+      "v_src":"https://player.vimeo.com/video/299705175"
+    },
+
+    {
+
+      "v_titles": "cara wittenhaus",
+      "v_description": "This is a place to write down, share and let go of all the hateful thoughts that you have. It felt relieving to me to write down these thoughts and give them a place to be.Unfiltered and unedited. together we create a place to archive (y(our)) hate",
+
+      "v_src":"https://docs.google.com/document/d/e/2PACX-1vT0F9ltwL7CBgQLiuH6kfTPQ___Ba_xW7C9DqBZmcHUVg6Zt-g4Ew6z92jRzZ77xuSXg9kKQFnrcH-Z/pub?embedded=true"
+    },
+
+    {
+
+      "v_titles": "cheawon shin",
+      "v_description": "Cheawon Shin and Sumin Choi made this work and all right reserved to them. / By using our magic power, we want to give a fascinating world to people.",
+      "v_src":"https://www.youtube.com/embed/3CEeWTfgNSw"
+    },
+
+    {
+        "v_titles": "xiaoyuan gao",
+        "v_description": "In this video, it talked about a person named Lily,  a photographer who takes photos every day... ",
+        "v_src":"https://www.youtube.com/embed/1wFbWfACKco",
+   },
+
+        {
+
+         "v_titles": "kuan-ting chen",
+           "v_description": "Typo-Projection method",
+            "v_src":"https://www.youtube.com/embed/k4F6TTZSZNM"
+          },
+
+
+
+          {
+
+            "v_titles": "lu lin",
+            "v_description": "The project “me, object” is an interactive game which consists of the processes of exploring personality and identity. With this game I want to trigger the participants to experience a journey of self-discovery. In the game, each person will individually question their openness, sense of beauty, joy, emotion, value for fashion, trust and examine their own ego.",
+            "v_src":"https://www.youtube.com/embed/TnL3EUm3fDw"
+          }
+
+
+          //{
+
+          //  "v_titles": "kuan-ting chen",
+          //  "v_description": "Puntuations as soil, and let the alphabets grow above. Punctuations as soil, and let the alphabets grow above.",
+          //  "v_src":"websites/kc/sr_twigs.html"
+          //}
+
+
+
+
+    ]
+}
+
+var picsdata = {
+  "photos":[
+
+    {
+
+      "p_images": "alexa3_Bin_Han.png",
+      "p_descriptions": "不窺牖, 見天道. 其出彌遠, 其知彌少, 문 밖을 나서지 않고도 세상을 알고, 창 밖을 내다보지 않고도 하늘의 도를 본다. 멀리 나갈수록 더욱 적게 안다고 함미당, Let's say be Alexa, Siri, and Geenie and L said, Knowing the world without leaving the door, Without looking out the window, I see the sky. The farther I go, the less I know. this webpage is great tho!",
+      "p_titles": "dongbin han"
+    },
+
+    {   "p_images": "530-Benjamin_McMillan.png",
+        "p_descriptions": "A Typeclub 530 workshop (http://fivethirty.today), an suggestion of how typefaces could be autocompleted",
+        "p_titles": "benjamin mcMillan"
+
+},
+
+    {   "p_images": "wearingmatters.png",
+        "p_descriptions": "living and wearing",
+        "p_titles": "xiaoyuan gao",
+
+
+      },
+
+
+    {   "p_images": "a_f.jpg",
+        "p_descriptions": "a practice about creating landscapes, mix the daily objects together, to generate images.",
+        "p_titles": "xiaoyuan gao",
+
+
+      },
+
+    {   "p_images": "human.jpg",
+        "p_descriptions": "what kind of human can i make?",
+        "p_titles": "xiaoyuan gao",
+
+
+      }
+    ]
+}
